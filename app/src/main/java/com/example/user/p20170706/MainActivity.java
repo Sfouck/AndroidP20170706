b7c8fffdfb67ead64c68636062bcd138352c54e5
Sfouck <wjcf129@gmail.com>
2017-07-06 16:47:48 +0800

    first 0706

--- app/src/main/java/com/example/user/p20170706/MainActivity.java
+++ app/src/main/java/com/example/user/p20170706/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.user.p20170706;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView myListView;
+    private String[] items = {"foo","bar"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,items);
+
+        myListView = (ListView) findViewById(R.id.myListView);
+        myListView.setAdapter(adapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object listItem = myListView.getItemAtPosition(position);
+                Toast.makeText(view.getContext(), listItem.toString(),Toast.LENGTH_SHORT).
+                        show();
+            }
+        });
     }
 }
